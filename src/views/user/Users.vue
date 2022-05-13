1a4876f63375e732e2fac12d014af9e603ad6005
suisuiz <wangyumin1996@gmail.com>
2022-05-13 18:34:46 +0800

    feat: user

---
+++ src/views/user/Users.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-05-13 18:31:38
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-05-13 18:32:06
+ * @FilePath: \Mall-system\src\views\user\Users.vue
+-->
+<template>
+  <div></div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+<style lang="scss" scoped></style>
