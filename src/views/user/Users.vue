4a871a2b889ad944aea6809bf917fe72aa4c631f
suisuiz <wangyumin1996@gmail.com>
2022-05-14 22:04:30 +0800

    feat: add

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -3,17 +3,23 @@
  * @Author: SUI
  * @Date: 2022-05-13 18:31:38
  * @LastEditors: SUI
- * @LastEditTime: 2022-05-14 22:02:45
+ * @LastEditTime: 2022-05-14 22:04:06
  * @FilePath: \Mall-system\src\views\user\Users.vue
 -->
 <template>
   <div>User Page</div>
 </template>
+//
 <script>
-import { defineComponent } from '@vue/composition-api'
+// import { defineComponent } from '@vue/composition-api'
 
-export default defineComponent({
-  setup() {}
-})
+// export default defineComponent({
+//   setup() {}
+// })
+//
+</script>
+
+<script>
+export default {}
 </script>
 <style lang="scss" scoped></style>
