ed617cabed4006e73b69c9852bae43a33161887e
suisuiz <wangyumin1996@gmail.com>
2022-05-15 17:03:47 +0800

    feat: add Bread components

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -3,11 +3,14 @@
  * @Author: SUI
  * @Date: 2022-05-13 18:31:38
  * @LastEditors: SUI
- * @LastEditTime: 2022-05-15 17:01:37
+ * @LastEditTime: 2022-05-15 17:02:44
  * @FilePath: \Mall-system\src\views\user\Users.vue
 -->
 <template>
-  <div>User Page</div>
+  <div>
+    <!-- 面包屑导航 -->
+    <Bread :title="breadTitle"></Bread>
+  </div>
 </template>
 <script>
 export default {
@@ -16,6 +19,15 @@ export default {
 </script>
 
 <script>
-export default {}
+// 引入面包屑
+import Bread from '@/components/common/Bread'
+export default {
+  name: 'Users',
+  // 引入模板
+  components: {
+    Bread
+  },
+  data() {}
+}
 </script>
 <style lang="scss" scoped></style>
