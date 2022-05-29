9d755d78142b33f8d444cb5a271065d25cfb5d65
suisuiz <wangyumin1996@gmail.com>
2022-05-29 18:16:47 +0800

    add new page

--- src/views/power/Rights.vue
+++ src/views/power/Rights.vue
@@ -6,3 +6,16 @@
  * @LastEditTime: 2022-05-29 18:13:17
  * @FilePath: \Mall-system\src\views\power\Rights.vue
 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+
+<style scoped></style>
--- src/views/power/Roles.vue
+++ src/views/power/Roles.vue
@@ -3,6 +3,19 @@
  * @Author: SUI
  * @Date: 2022-05-29 18:13:04
  * @LastEditors: SUI
- * @LastEditTime: 2022-05-29 18:13:04
+ * @LastEditTime: 2022-05-29 18:16:27
  * @FilePath: \Mall-system\src\views\power\Roles.vue
 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+
+<style scoped></style>
