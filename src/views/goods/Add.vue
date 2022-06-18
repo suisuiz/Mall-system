54a2f61835b1175db8255559ca37fc3af2519577
suisuiz <wangyumin1996@gmail.com>
2022-06-18 22:57:51 +0800

    feat: new branch

---
+++ src/views/goods/Add.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-06-18 22:56:44
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-06-18 22:56:45
+ * @FilePath: \Mall-system\src\views\goods\Add.vue
+-->
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'Add',
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
