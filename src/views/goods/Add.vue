4824bc89de21795d87dc507fdfb258879870c1ce
suisuiz <wangyumin1996@gmail.com>
2022-06-19 22:48:53 +0800

    update

--- src/views/goods/Add.vue
+++ src/views/goods/Add.vue
@@ -3,11 +3,17 @@
  * @Author: SUI
  * @Date: 2022-06-18 22:56:44
  * @LastEditors: SUI
- * @LastEditTime: 2022-06-18 22:56:45
+ * @LastEditTime: 2022-06-19 22:48:18
  * @FilePath: \Mall-system\src\views\goods\Add.vue
 -->
 <template>
-  <div></div>
+  <div>
+    <!-- 面包屑导航 -->
+    <Bread :title="breadTitle"></Bread>
+
+    <!-- 内容展示卡片 -->
+    <el-card class="box-card"> </el-card>
+  </div>
 </template>
 <script>
 export default {
