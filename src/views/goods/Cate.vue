79e50f90433aaf351583b668f6d88a4defabcb41
suisuiz <wangyumin1996@gmail.com>
2022-06-20 09:26:48 +0800

    update

---
+++ src/views/goods/Cate.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-06-20 09:26:26
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-06-20 09:26:26
+ * @FilePath: \Mall-system\src\views\goods\Cate.vue
+-->
