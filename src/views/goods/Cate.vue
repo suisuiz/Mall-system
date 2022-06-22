1a4d6acec813f971c818df52fa6dfab68db30544
suisuiz <wangyumin1996@gmail.com>
2022-06-22 16:49:18 +0800

    update

--- src/views/goods/Cate.vue
+++ src/views/goods/Cate.vue
@@ -3,6 +3,22 @@
  * @Author: SUI
  * @Date: 2022-06-20 09:26:26
  * @LastEditors: SUI
- * @LastEditTime: 2022-06-20 09:26:26
+ * @LastEditTime: 2022-06-22 16:48:24
  * @FilePath: \Mall-system\src\views\goods\Cate.vue
 -->
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
