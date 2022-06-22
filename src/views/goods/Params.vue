35f0e4d4b27e08eb83f7db878c7d487d754cd0cd
suisuiz <wangyumin1996@gmail.com>
2022-06-22 16:55:18 +0800

    feat: new page

---
+++ src/views/goods/Params.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-06-22 16:55:01
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-06-22 16:55:01
+ * @FilePath: \Mall-system\src\views\goods\Params.vue
+-->
