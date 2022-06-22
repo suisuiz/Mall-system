8461c50d27f029168ca382f95fd3f19cc7d74cf8
suisuiz <wangyumin1996@gmail.com>
2022-06-22 16:54:32 +0800

    new page

---
+++ src/views/goods/Goods.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-06-22 16:54:14
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-06-22 16:54:15
+ * @FilePath: \Mall-system\src\views\goods\Goods.vue
+-->
