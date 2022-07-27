0a6780b63377e00989c26646b9e045d9ee0d2859
suisuiz <wangyumin1996@gmail.com>
2022-07-27 14:13:23 +0800

    add

--- src/views/order/Orders.vue
+++ src/views/order/Orders.vue
@@ -3,7 +3,7 @@
  * @Author: SUI
  * @Date: 2022-07-19 14:40:59
  * @LastEditors: SUI
- * @LastEditTime: 2022-07-27 14:09:22
+ * @LastEditTime: 2022-07-27 14:12:19
  * @FilePath: \Mall-system\src\views\order\Orders.vue
 -->
 <template>
@@ -12,7 +12,19 @@
     <Bread :title="breadTitle"></Bread>
 
     <!-- 内容展示卡片 -->
-    <el-card class="box-card"> </el-card>
+    <el-card class="box-card">
+      <!-- 搜索框 -->
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getOrderList">
+            <el-button slot="append" icon="el-icon-search" @click="getOrderList"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <!-- 内容 -->
+      <!-- 订单列表数据 -->
+    </el-card>
   </div>
 </template>
 
