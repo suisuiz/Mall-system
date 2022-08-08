a202e54a356ca9366a8360db08bb4a253c1715c7
suisuiz <wangyumin1996@gmail.com>
2022-08-08 18:51:25 +0800

    add Reports.vue

---
+++ src/views/report/Reports.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Descripttion: 
+ * @Author: SUI
+ * @Date: 2022-08-08 18:50:58
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-08-08 18:50:58
+ * @FilePath: \Mall-system\src\views\report\Reports.vue
+-->
