36ec6c9822347318ac66771ff87bac1629ba6a82
suisuiz <wangyumin1996@gmail.com>
2022-08-10 09:33:34 +0800

    feat: add bread

--- src/views/report/Reports.vue
+++ src/views/report/Reports.vue
@@ -3,16 +3,24 @@
  * @Author: SUI
  * @Date: 2022-08-08 18:50:58
  * @LastEditors: SUI
- * @LastEditTime: 2022-08-10 09:31:45
+ * @LastEditTime: 2022-08-10 09:33:13
  * @FilePath: \Mall-system\src\views\report\Reports.vue
 -->
 <template>
-  <div></div>
+  <div>
+    <!-- 面包屑导航 -->
+    <Bread :title="breadTitle"></Bread>
+  </div>
 </template>
 
 <script>
+// 引入面包屑
+import Bread from '@/components/common/Bread'
 export default {
   name: 'Reports',
+
+  components: { Bread },
+
   data() {
     return {}
   },
