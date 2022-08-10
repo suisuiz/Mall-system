7f45240f0ae2976fae6c4acdced01e52bbd426e2
suisuiz <wangyumin1996@gmail.com>
2022-08-10 09:32:14 +0800

    new branch

--- src/views/report/Reports.vue
+++ src/views/report/Reports.vue
@@ -3,6 +3,24 @@
  * @Author: SUI
  * @Date: 2022-08-08 18:50:58
  * @LastEditors: SUI
- * @LastEditTime: 2022-08-08 18:50:58
+ * @LastEditTime: 2022-08-10 09:31:45
  * @FilePath: \Mall-system\src\views\report\Reports.vue
 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Reports',
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
