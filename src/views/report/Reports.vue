e69665c64f1af9aaf06dfedde62ead9ca9a86e65
suisuiz <wangyumin1996@gmail.com>
2022-08-11 08:47:27 +0800

    echarts

--- src/views/report/Reports.vue
+++ src/views/report/Reports.vue
@@ -3,13 +3,20 @@
  * @Author: SUI
  * @Date: 2022-08-08 18:50:58
  * @LastEditors: SUI
- * @LastEditTime: 2022-08-10 09:34:22
+ * @LastEditTime: 2022-08-11 08:46:21
  * @FilePath: \Mall-system\src\views\report\Reports.vue
 -->
 <template>
   <div>
     <!-- 面包屑导航 -->
     <Bread :title="breadTitle"></Bread>
+
+    <!-- 内容展示卡片 -->
+    <el-card class="box-card">
+      <!-- 渲染 echarts 图表 -->
+      <!-- 为 echarts 准备一个具备大小（宽、高）的 DOM -->
+      <div id="echartsBox" style="width: 800px; height: 400px"></div>
+    </el-card>
   </div>
 </template>
 
