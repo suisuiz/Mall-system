6e95d7b81c1ad48999ba4dcc415fc0a5666b64d0
suisuiz <wangyumin1996@gmail.com>
2022-08-11 08:48:47 +0800

    feat:echarts data

--- src/views/report/Reports.vue
+++ src/views/report/Reports.vue
@@ -43,6 +43,37 @@ export default {
         one: '数据统计',
         two: '数据报表',
       },
+      // 要合并的数据
+      options: {
+        title: {
+          text: '用户来源',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3',
+            },
+          },
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            boundaryGap: false,
+          },
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          },
+        ],
+      },
     }
   },
 
