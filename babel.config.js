9de94d2a33f218cc56359ed91a441c7787dbb478
suisuiz <wangyumin1996@gmail.com>
2022-08-19 09:21:05 +0800

    update package

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,32 @@
+/*
+ * @Descripttion:
+ * @Author: SUI
+ * @Date: 2022-05-09 12:12:01
+ * @LastEditors: SUI
+ * @LastEditTime: 2022-08-18 18:54:30
+ * @FilePath: \Mall-system\babel.config.js
+ */
+// 这是在项目发布阶段需要用到的babel插件
+const prodPlugins = []
+
+// 判断是发布模式
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  "plugins": [
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
     [
-      "component",
+      'component',
       {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ]
-  ]
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk',
+      },
+    ],
+    // 发布产品时候的插件组件
+    ...prodPlugins,
+    // 路由懒加载配置
+    '@babel/plugin-syntax-dynamic-import',
+  ],
 }
