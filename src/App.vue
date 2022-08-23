8ceb9850f44e06001c0dfb70f0ea8928df7b9afe
suisuiz <wangyumin1996@gmail.com>
2022-08-23 19:31:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
  * @Author: SUI
  * @Date: 2022-05-09 12:12:01
  * @LastEditors: SUI
- * @LastEditTime: 2022-05-13 18:24:55
+ * @LastEditTime: 2022-08-23 19:29:40
  * @FilePath: \Mall-system\src\App.vue
 -->
 <template>
